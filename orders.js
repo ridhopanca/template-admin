67354afdca60d594c820b88b36f26987f7043e3d
Ridho Panca <pancasakti08@gmail.com>
2022-05-30 03:02:20 +0700

    end of learning yt

---
+++ orders.js
@@ -0,0 +1,50 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending' 
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending' 
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36374',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined' 
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49437',
+        paymentStatus: 'Due',
+        shipping: 'Pending' 
+    },
+    {
+        productName: 'Drone With Camera Drone',
+        productNumber: '12025',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered' 
+    },
+    {
+        productName: 'GPS K211 Drone',
+        productNumber: '28821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered' 
+    },
+    {
+        productName: 'DJI Air 425',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending' 
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '80845',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered' 
+    },
+]
